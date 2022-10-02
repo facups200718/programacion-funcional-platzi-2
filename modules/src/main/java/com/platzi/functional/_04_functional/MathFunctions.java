7564f9abde808fe3697a0d89d48e3f1c35a0cb53
Facundo Perez Schneider <facup200718@gmail.com>
2022-10-02 17:56:43 -0300

    Clase 12

--- modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -1,6 +1,7 @@
 package com.platzi.functional._04_functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
     public static void main(String[] args) {
@@ -12,5 +13,25 @@ public class MathFunctions {
         };
         System.out.println(square.apply(5));
         System.out.println(square.apply(25));
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        isEven.test(4);
+
+        Predicate<Student> isApproved = student -> student.getGrade() >= 6;
+        System.out.println(isApproved.test(new Student(5)));
+    }
+
+    static class Student {
+        private double grade;
+
+        public double getGrade() {
+            return grade;
+        }
+
+        public Student(double grade) {
+            this.grade = grade;
+        }
     }
 }
